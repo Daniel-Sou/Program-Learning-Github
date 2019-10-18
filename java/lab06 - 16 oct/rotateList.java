0cc70fa806fedcfd2d2727494529b73dab299cf0
Daniel Sou <danielsou0622@gmail.com>
2019-10-18 15:40:37 +0800

    update

--- java/lab06 - 16 oct/rotateList.java
+++ java/lab06 - 16 oct/rotateList.java
@@ -14,5 +14,20 @@ public class rotateList {
     int size = 0;
 
     // Represent the head and tail of the doubly linked list
-    Node
+    Node head, tail = null;
+
+    // addNode() will add a node to the list
+    public void addNode(int data) {
+        // Create a new node
+        Node newNode = new Node(data);
+
+        // If list is emply
+        if (head == null) {
+            // Both head and tail will point to newNode
+            head = tail = newNode;
+
+            // Head's previous will point to null
+            head.previous = null;
+        }
+    }
 }
