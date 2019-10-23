1d2bc171ee53ed28c3affc7b42f9f2f3a01697eb
Daniel_Sou <bb70272@umac.mo>
2019-10-23 11:07:27 +0800

    updated by Cloud Studio

--- java/lab07/fibLinear.java
+++ java/lab07/fibLinear.java
@@ -11,7 +11,7 @@ public class fibLinear {
   public static void main(String[] args) {
     int n = Integer.parseInt(args[0]);
     for ( int i=1; i <=n; i++) {
-      System.out.println(i + ": " + fibLinear(i));
+      System.out.println(i + ": " + fibLinear(1,1,i));
     }
   }
 
