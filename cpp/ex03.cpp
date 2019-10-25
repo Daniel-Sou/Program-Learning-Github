d96bfd643e6754ab23ff2dfc8a24c3f59f3eeab3
Daniel Sou <danielsou0622@gmail.com>
2019-10-25 11:07:28 +0800

    update ex03 cpp

---
+++ cpp/ex03.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+    int n1 = 10;
+    int n2 = 10;
+
+    // While-loop 1
+    while(n1 > 0) {
+        n1 /= 2;
+    }
+
+    // "\n" means hold a new line
+    std::cout << n1 * n1 << "\n";
+
+    // While-loop 2
+    while (n2 > 0) {
+        n2 /= 2;
+        // "\t" means hold a tab
+        std::cout << n2 * n2 << "\t";
+    }
+
+    std::cout << "\n";
+    
+    return 0;
+}
