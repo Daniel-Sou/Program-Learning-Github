b150614dfcb01c15e451c2abdcfc44585ff0e6b4
Daniel Sou <danielsou0622@gmail.com>
2019-10-29 02:10:20 +0800

    update

--- java/review/InterfaceDemo.java
+++ java/review/InterfaceDemo.java
@@ -12,6 +12,8 @@
 
 import java.util.*;
 
+import javafx.scene.shape.Rectangle;
+
 class InterfaceDemo {
     public static void main(String[] args) {
 
@@ -34,7 +36,45 @@ class InterfaceDemo {
         b = y.nextInt();
 
         Circle Circle1 = new Circle(r);
-        Rectangle rectangle1 = new 
+        Rectangle rectangle1 = new Rectangle(a, b);
+        Circle1.area();
+        rectangle1.area();
+    }
+}
 
+abstract class ShapeArea {
+    abstract void area();
+}
+
+class Circle extends ShapeArea {
+    double PI = 3.14;
+
+    double radius;
+
+    public Circle(double r) {
+        radius = r;
+    }
+
+    public void area() {
+        double s1 = radius * radius * PI;
+        System.out.println("The area of circle is " + s1);
+    }
+}
+
+class Rectangle extends ShapeArea {
+    // Fields
+    double height;
+    double weight;
+
+    // Constructors
+    public Rectangle(double a, double b) {
+        height = a;
+        weight = b;
+    }
+
+    // Method
+    public void area() {
+        double s2 = weight * height;
+        System.out.println("The area of the rectangle is " + s2);
     }
 }
