99e9f1232c1469531ab94bea26c020cdb4982cf2
Daniel_Sou <bb70272@umac.mo>
2019-11-13 17:32:25 +0800

    13/11 Stack - Java

--- java/lab08/ArrayStackDemo.java
+++ java/lab08/ArrayStackDemo.java
@@ -13,6 +13,47 @@ public class ArrayStackDemo{
     while(flag) {
       System.out.println("Show >> ");
       System.out.println("Exit >> ");
+      System.out.println("Push >> ");
+      System.out.println("Pop >> ");
+      key = sc.nextLine();
+
+      switch (key) {
+        case "show":
+          a.show();
+          break;
+        
+        case "exit":
+          flag = false;
+          System.out.println("The Program is exit!! ");
+          break;
+        
+        case "push":
+          System.out.println("Insert the data to stack >> ");
+          int val = sc.nextInt();
+          a.push(val);
+        
+        case "pop":
+          int pop = a.pop();
+          System.out.println("The value of POP out >> " + pop);
+      }
     }
   }
+}
+
+class ArrayStack{
+  private int MaxSize;
+  private int[] arr;
+  private int top = -1;
+  
+  public ArrayStack(int maxSize) {
+    this.MaxSize = maxSize;
+    arr = new int[MaxSize];
+  }
+
+  //
+  public boolean isEmpty() {
+    return top == -1;
+  }
+
+  public 
 }
