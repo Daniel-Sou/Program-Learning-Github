c66ba51f9f4a50407102c41b5d66a15015781dd7
Daniel_Sou <bb70272@umac.mo>
2019-11-13 17:15:22 +0800

    updated by Cloud Studio

---
+++ java/lab08/ArrayStackDemo.java
@@ -0,0 +1,11 @@
+// Java Program - Stack Implement with array
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class ArrayStackDemo{
+  public static void main(String[] args){
+    ArrayStack a = new ArrayStack(5);
+
+  }
+}
