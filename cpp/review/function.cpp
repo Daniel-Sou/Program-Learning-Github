94e6f9a079aa6de959489385c4fcbf534c875546
Daniel Sou <danielsou0622@gmail.com>
2019-12-18 00:51:07 +0800

    update

---
+++ cpp/review/function.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+long xfunction(long n) {
+    cout << "long >> ";
+    return n;
+}
+
+int xfunction(int n, long t) {
+    cout << "int >> ";
+    return t - n;
+}
+
+short xfunction(short m, long n, int p) {
+    cout << "long >> ";
+    return p + n + m;
+}
+
+int main() {
+    cout << xfunction(5) << endl;
+    cout << xfunction(5,10) << endl;
+    cout << xfunction(5,10,15) << endl;
+}
---
+++ cpp/review/xfunction.cpp
@@ -0,0 +1,1 @@
+#include "xfunction.h"
